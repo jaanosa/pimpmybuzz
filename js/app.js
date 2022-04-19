477a197d33377dc5e06878aa19c94722ba4babeb
jaanosa11 <jaanosa11@gmail.com>
2022-04-19 21:36:08 +0800

    Update Email function

--- js/app.js
+++ js/app.js
@@ -1,3 +1,10 @@
+let message = {
+    'status' : null,
+    'msg' : null
+};
+let alert       = null;
+let alert_body  = null;
+
 $(document).ready(function () {
     // initialize animation on scroll
     AOS.init();
@@ -26,39 +33,88 @@ $(window).on('scroll resize', function () {
 });
 
 function initEmail() {
-    let submit = $('#contact-us-btn');
+    let submit      = $('#contact-us-btn');
+    let name        = $('#inp_name');
+    let email       = $('#inp_email');
+    let msg         = $('#inp_msg');
+    let data        = [];
+    alert           = $('#alert-msgs');    
+    alert_body      = $('.alert-body');
+
     if(submit.length > 0){
         submit.on('click',function(){
-            sendEmail();
+            if(email.val() != ''){
+                data = {
+                    'name'  : name.val(),
+                    'email' : email.val(),
+                    'msg'   : msg.val(),
+                }
+                sendEmail(data);
+            }else{
+                message = {
+                    'status': 'error',
+                    'msg'   : 'Email field is required.'
+                }
+                alert_body.html('');
+                alert_body.html(message.msg);
+                alert.removeClass('alert-success');
+                alert.removeClass('alert-danger');
+                alert.addClass('alert-danger');
+                alert.toggleClass('show');
+            }
         });
     }
 }
 
-function sendEmail() {
-    // Email.send({
-    //   Host: "smtp.gmail.com",
-    //   Username: "pimpmybuzz2022@gmail.com",
-    //   Password: "PimpMyBuzz_2022",
-    //   To: 'jaanosa11@gmail.com',
-    //   From: "pimpmybuzz2022@gmail.com",
-    //   Subject: "Sending Email using javascript",
-    //   Body: "Well that was easy!!",
-    // })
-    //   .then(function (message) {
-    //     alert(message);
-    //     // alert("mail sent successfully")
-    //   });
+function sendEmail(data) {
+    alert       = $('#alert-msgs');
+    alert_body  = $('.alert-body');
+    let submit  = $('.submit-wrap');
+    let load    = $('.loading-wrap');
+
+    submit.toggleClass('d-none');
+    load.toggleClass('d-none');
+
     Email.send({
         // SecureToken : "f865c869-11c9-46ad-9663-ed95629af79f",
         Host: "smtp.gmail.com",
         Username: "pimpmybuzz2022@gmail.com",
         Password: "PimpMyBuzz_2022",
         To : 'jaanosa11@gmail.com',
-        From : "jessie.allen.anosa@acret-ph.com",
-        Subject : "This is the subject",
-        Body : "And this is the body"
+        From : data.email,
+        Subject : `Inquiry from ${data.name}`,
+        Body : data.msg
     }).then(
-      message => alert(message)
+      messages => {
+        let status = null;
+        let msgg   = null;
+        message = {
+            'status': null,
+            'msg'   : null
+        }
+        console.log(messages);
+        if(messages != 'OK'){
+            status = 'error';
+            msgg   = messages;
+        }else{
+            status = 'success';
+            msgg   = 'Email successfully sent.'
+        }
+        message = {
+            'status': status,
+            'msg'   : msgg
+        }
+
+        alert_body.html('');
+        alert_body.html(message.msg);
+        alert.removeClass('alert-success');
+        alert.removeClass('alert-danger');
+        alert.addClass(message.status != 'success' ? 'alert-danger' : 'alert-success');
+        alert.toggleClass('show');
+
+        submit.toggleClass('d-none');
+        load.toggleClass('d-none');
+      } 
     );
 }
 
