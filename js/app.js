05939c32ad0b6597de88a0177974d1ebae327ebd
jaanosa11 <58842690+jaanosa11@users.noreply.github.com>
2022-04-19 12:17:57 +0800

    Add animation in all pages

--- js/app.js
+++ js/app.js
@@ -1,4 +1,20 @@
 $( document ).ready(function() {
+    // initialize animation on scroll
     AOS.init();
+
+    //initialize swiper
+    initSwiper();
 });
+
+function initSwiper(){
+    var swiper = new Swiper(".mySwiper", {
+        spaceBetween: 30,
+        centeredSlides: true,
+        loop: true,
+        autoplay: {
+          delay: 2500,
+          disableOnInteraction: false,
+        },
+      });
+}
   
