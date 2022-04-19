f0c15b49c6687fb5fb4686311baa728664ae7d06
jaanosa11 <58842690+jaanosa11@users.noreply.github.com>
2022-04-19 13:55:35 +0800

    Update header and add mobile design

--- js/app.js
+++ js/app.js
@@ -1,20 +1,48 @@
-$( document ).ready(function() {
+$(document).ready(function () {
     // initialize animation on scroll
     AOS.init();
 
     //initialize swiper
     initSwiper();
+
+    //initialize mobile hamburger menu
+    setNav();
 });
 
-function initSwiper(){
+$(window).on('scroll resize', function () {
+    let toggled = $('.toggle-nav');
+    let hamburger = $('.nav-hamburger'),
+        mobile_nav = $('.mobile-nav'),
+        body = $('body');
+
+    if(toggled.length > 0){
+        $(mobile_nav).toggleClass('toggle-nav');
+        $(hamburger).toggleClass('toggle-menu');
+        $(body).toggleClass('toggle-body');
+    }
+});
+
+function initSwiper() {
     var swiper = new Swiper(".mySwiper", {
         spaceBetween: 30,
         centeredSlides: true,
         loop: true,
         autoplay: {
-          delay: 2500,
-          disableOnInteraction: false,
+            delay: 2500,
+            disableOnInteraction: false,
         },
-      });
+    });
 }
-  
+
+//for menu toggle
+function setNav() {
+    let hamburger = $('.nav-hamburger'),
+        mobile_nav = $('.mobile-nav'),
+        body = $('body');
+
+    $(hamburger).on('click', function () {
+        $(mobile_nav).toggleClass('toggle-nav');
+        $(hamburger).toggleClass('toggle-menu');
+        $(body).toggleClass('toggle-body');
+    });
+}
